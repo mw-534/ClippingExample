657f6cefaf43f7662c5c5ecdeb2985940fe88fcd
ASUS <m>
2021-11-03 14:51:10 +0100

    Set up row and column locations.

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -5,7 +5,6 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.res.ResourcesCompat
 
 class ClippedView @JvmOverloads constructor(
     context: Context,
@@ -22,7 +21,7 @@ class ClippedView @JvmOverloads constructor(
 
     private var path = Path()
 
-    private val clipRightRect = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
     private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
     private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
     private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
@@ -34,4 +33,13 @@ class ClippedView @JvmOverloads constructor(
 
     private val textOffset = resources.getDimension(R.dimen.textOffset)
     private val textSize = resources.getDimension(R.dimen.textSize)
+
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRight
+
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
 }
